4f00c1ad4a971b8877d7762f7f3bc8c960d9a73f
marthak406 <marthakristiana406@gmail.com>
2024-04-23 10:58:25 +0700

    update data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,6 +10,8 @@ use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
 //import Facade "Validator"
 use Illuminate\Support\Facades\Validator;
+//import Facade "Storage"
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -55,5 +57,73 @@ class PostController extends Controller
 
         //return response
         return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
+    }
+
+    /**
+     * show
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    public function show($id)
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //return single post as a resource
+        return new PostResource(true, 'Detail Data Post!', $post);
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $post
+     * @return void
+     */
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //check if image is not empty
+        if ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/'.basename($post->image));
+
+            //update post with new image
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+
+        } else {
+
+            //update post without image
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
     }
 }
