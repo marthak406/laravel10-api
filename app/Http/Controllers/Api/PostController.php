9ad43cfa396b82419d04242c9caa328acad72b19
marthak406 <marthakristiana406@gmail.com>
2024-04-23 10:15:41 +0700

    insert data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 //import Resource "PostResource"
 use App\Http\Resources\PostResource;
+//import Facade "Validator"
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -18,5 +20,40 @@ class PostController extends Controller
 
         //return collection of posts as a resource
         return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content,
+        ]);
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
 }
